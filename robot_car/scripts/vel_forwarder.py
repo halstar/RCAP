21959c3835d13b3b53e7de2fb30569e84a09a0cb
halstar <halstar@hotmail.com>
2022-05-02 22:26:35 +0200

    Starting working on velocity control from ROS

--- robot_car/scripts/vel_forwarder.py
+++ robot_car/scripts/vel_forwarder.py
@@ -1,30 +1,40 @@
 #!/usr/bin/env python3
 
 import rclpy
-from rclpy.node import Node
 
-from std_msgs.msg import String
+from rclpy.node        import Node
+from geometry_msgs.msg import Twist
 
+WHEEL_RADIUS            = 0.040
+WHEEL_SEPARATION_WIDTH  = 0.195
+WHEEL_SEPARATION_LENGTH = 0.175
 
 class VelocityForwarder(Node):
 
     def __init__(self):
         super().__init__('vel_forwarder')
 
-        self.get_logger().info("Starting Velocity Forwarder Node");
+        self.get_logger().info('Starting Velocity Forwarder Node');
 
         self.subscription = self.create_subscription(
-            String,
-            'topic',
-            self.listener_callback,
-            10)
+            Twist,
+            'cmd_vel',
+            self.handle_message,
+            1)
         self.subscription  # prevent unused variable warning
 
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+    def handle_message(self, msg):
+
+        wheel_front_left  = (1 / WHEEL_RADIUS) * (msg.linear.x - msg.linear.y - (WHEEL_SEPARATION_WIDTH + WHEEL_SEPARATION_LENGTH) * msg.angular.z)
+        wheel_front_right = (1 / WHEEL_RADIUS) * (msg.linear.x + msg.linear.y + (WHEEL_SEPARATION_WIDTH + WHEEL_SEPARATION_LENGTH) * msg.angular.z)
+        wheel_rear_left   = (1 / WHEEL_RADIUS) * (msg.linear.x + msg.linear.y - (WHEEL_SEPARATION_WIDTH + WHEEL_SEPARATION_LENGTH) * msg.angular.z)
+        wheel_rear_right  = (1 / WHEEL_RADIUS) * (msg.linear.x - msg.linear.y + (WHEEL_SEPARATION_WIDTH + WHEEL_SEPARATION_LENGTH) * msg.angular.z)
+
+        self.get_logger().info('F.L.: {:.2f} - F.R.: {:.2f} - R.L.: {:.2f} - R.R.: {:.2f}'.format(wheel_front_left, wheel_front_right, wheel_rear_left, wheel_rear_right))
 
 
 def main(args=None):
+
     rclpy.init(args=args)
 
     vel_forwarder = VelocityForwarder()
@@ -32,6 +42,7 @@ def main(args=None):
     rclpy.spin(vel_forwarder)
 
     vel_forwarder.destroy_node()
+
     rclpy.shutdown()
 
 
