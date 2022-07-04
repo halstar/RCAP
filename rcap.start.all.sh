452a69c21ba82fbd1ae1f4035ecf6667f0203212
halstar <halstar@hotmail.com>
2022-07-04 22:23:14 +0200

    Putting it all together

--- rcap.start.all.sh
+++ rcap.start.all.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+cd mpu9250
+source install/setup.bash 
+
+cd ..
+cd robot_car
+source install/setup.bash 
+
+cd ..
+cd rplidar
+source install/setup.bash 
+
+cd ..
+source install/setup.bash 
+
 ros2 launch rcap rcap.physical.launch.py | grep -v full
